df447a65c9fbc6918e45eb0bef6d77e9bb4e6a8a
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-04-10 16:42:37 -0400

    mostly finished

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,41 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { render } from "@testing-library/react";
+const axios = require('axios');
 
 function App() {
-  const form: HTMLFormElement = document.querySelector('#defineform');
+
+  const [def, setDef] = useState([]);
 
 
-form.onsubmit = () => {
-  const formData = new FormData(form);
 
-  console.log(formData);
-  const text = formData.get('defineword') as string;
-  console.log(text);
-  return false; // prevent reload
-};
+  const form: HTMLFormElement = document.querySelector("#defineform");
+  document.body.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    let form = event.target as HTMLFormElement;
+  });
+
+
+
+    useEffect(() => {
+      const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+      form.onsubmit = async () => {
+        const formData = new FormData(form);
+        // console.log(formData);
+        const text = formData.get("defineword") as string;
+        axios.get(url + text)
+          .then((res) => {
+            console.log(res);
+            setDef(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+        })
+        //.then((response: Response) => response.json())
+        // const json = response.json();
+        // setDef(await json);
+        //.then(function (data) {
+        // console.log(json);
+        //  console.log(def);
+      }
+    }
+
+      //.catch((error) => console.log(error));
+      //console.log(text);
+
+      ,[]);
   return (
     <div className="App">
-      <main className="container">
-  <div className="bg-light p-5 rounded">
-    <h1>Definition</h1>
-    <p className="lead">Use this part of the page to present your results from the API call.</p>
+<main className="container">
+<div className="bg-light p-5 rounded">
+  <h1 className="defined">Dictionary</h1>
+  <p className="lead">Definition</p>
+  {def.map((def:any) => (
+  <ul className="list-unstyled">
+    <li>Word: {def.word}</li>
+    {def.phonetics.map((phonetics:any) =>(
     <ul className="list-unstyled">
-        <li>This is a list. Your word could go here.</li>
-        <li>It appears completely unstyled.</li>
-        <li>Maybe your multiple definitions of the word are here?</li>
-        <li>Structurally, it's still a list.</li>
-        <li>However, this style only applies to immediate child elements.</li>
-        <li>Nested lists: (maybe synonyms and antonyms?)
-          <ul>
-            <li>are unaffected by this style</li>
-            <li>will still show a bullet</li>
-            <li>and have appropriate left margin</li>
-          </ul>
-        </li>
-        <li>This may still come in handy in some situations.</li>
-      </ul>
-  </div>
+      <li>{phonetics.text}</li>
+    {def.meanings.map((meanings:any) => (
+    <ul className="list-unstyled">
+        <li>{meanings.partOfSpeach}</li>
+        <li>Synonym(s): {meanings.synonyms.join(', ')}</li>
+        <li>Antonym(s): {meanings.antonyms.join(', ')}</li>
+            {meanings.definitions.map((definitions: any) => (
+              <ul className="list-unstyled">
+                <ol>*{definitions.definition}</ol>
+              </ul>
+            ))}
+    </ul>
+    ))}
+    <p> ************************************************************************************</p>
+    </ul>
+    ))}
+    </ul>
+    ))}
+ </div>
+
 </main>
     </div>
   );
